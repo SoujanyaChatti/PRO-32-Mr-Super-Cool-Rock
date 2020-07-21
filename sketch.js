860dc670d1d0f5db685d4444378b5587408fbc25
SoujanyaChatti <67154494+SoujanyaChatti@users.noreply.github.com>
2020-07-21 15:39:55 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -12,7 +12,7 @@ const Constraint=Matter.Constraint;
 
  function preload(){
  
-   backgroundIMG=loadImage("sprites/daybg.jpg");
+   backgroundIMG=loadImage("sprites/nightbg.jpg");
    arrowim=loadImage("sprites/arrow.png");
    gettime();
  }
